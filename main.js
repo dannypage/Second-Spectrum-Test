4d2867152536f144cc15840df13d8222b82e90b3
Danny Page <daniel.w.page@gmail.com>
2015-04-10 17:00:54 -0400

    Movement and key presses being finalized to only fire once.

--- main.js
+++ main.js
@@ -17,43 +17,67 @@ var mainState = {
     // Here we set up the game, display sprites, etc.
         landscape.initMap();
 
+        selected = null;
+        isMoving = false;
+
         large = game.add.sprite(350, 350, 'large');
         medium = game.add.sprite(200, 200, 'medium');
         small = game.add.sprite(50, 50, 'small');
-        large.anchor.setTo(0.5, 0.5);
-        medium.anchor.setTo(0.5, 0.5);
-        small.anchor.setTo(0.5, 0.5);
+        large.anchor.setTo(0.5);
+        medium.anchor.setTo(0.5);
+        small.anchor.setTo(0.5);
+        large.inputEnabled = true;
+        medium.inputEnabled = true;
+        small.inputEnabled = true;
+        large.events.onInputDown.add(actions.select, this);
+        medium.events.onInputDown.add(actions.select, this);
+        small.events.onInputDown.add(actions.select, this);
         game.physics.arcade.enable(small);
         game.physics.arcade.enable(medium);
         game.physics.arcade.enable(large);
         cursors = game.input.keyboard.createCursorKeys();
 
         landscape.drawMap();
+        cursors.left.onDown.add(actions.moveDisk,{direction: 'left'});
     },
     update: function() {
     // This function is called 60 times per second
     // It contains the game's logic
-        actions.movePlayer();
+
     },
 };
 var actions = {
-    movePlayer: function() {
+    moveDisk: function(direction) {
+        if (selected != null) {
+            if (direction == 'left') {
+                selected.body.velocity.x += -50;
+            }
+        }
 
-        // If the left arrow key is pressed
-        if (cursors.left.isDown) {
-        // Move the player to the left
-            small.body.velocity.x = -200;
-        }
-        // If the right arrow key is pressed
-        else if (cursors.right.isDown) {
-        // Move the player to the right
-            small.body.velocity.x = 200;
-        }
-        // If neither the right or left arrow key is pressed
-        else {
-        // Stop the player
-            small.body.velocity.x = 0;
-        }
+
+            // (selected != null && !isMoving) {
+            // // If the left arrow key is pressed
+            // if (cursors.left.justPressed() ) {
+            // // Move the player to the left
+
+            //     selected.body.position.x += -50;
+            // }
+            // // If the right arrow key is pressed
+            // else if (cursors.right.justPressed() ) {
+            // // Move the player to the right
+
+            //     selected.body.position.x += 50;
+            // }
+            // // If neither the right or left arrow key is pressed
+            // else {
+            // // Stop the player
+            //     0;
+            // }
+        //}
+    },
+    select: function(disk) {
+        selected = disk;
+        console.log(disk);
     }
 };
 
@@ -78,7 +102,7 @@ var landscape = {
 }
 
 // We initialising Phaser
-var game = new Phaser.Game(1200, 600, Phaser.AUTO, 'gameDiv');
+var game = new Phaser.Game(1280, 600, Phaser.AUTO, 'gameDiv');
 // And finally we tell Phaser to add and start our 'main' state
 game.state.add('main', mainState);
 game.state.start('main');
