dcd645be45d1ffaf82cb4ca7c779da0dba0ace26
Danny Page <daniel.w.page@gmail.com>
2015-04-17 17:49:07 -0400

    Final variables. Time to write up results.

--- shot_percentages.rb
+++ shot_percentages.rb
@@ -18,7 +18,7 @@ def percentage(tracker_hash)
     if attempts > 0
         made.to_f / attempts
     else
-        0.5
+        0
     end
 end
 
@@ -26,6 +26,16 @@ location = Hash.new do |hash,key|
     hash[key] = Hash.new do |hash,key|
         hash[key] = {:attempts => 0, :made => 0}
     end
+end
+type_and_distance = Hash.new do |hash,key|
+    hash[key] = Hash.new do |hash,key|
+        hash[key] = {:attempts => 0, :made => 0}
+    end
+end
+shooter_and_type = Hash.new do |hash,key|
+  hash[key] = Hash.new do |hash,key|
+    hash[key] = {:attempts => 0, :made => 0}
+  end
 end
 shot_type = Hash.new {|h, k| h[k] = {:attempts => 0, :made => 0} }
 distance  = Hash.new {|h, k| h[k] = {:attempts => 0, :made => 0} }
@@ -38,17 +48,28 @@ test_game_counter = 0
 testing = false
 shot_counter = 0
 
+testing_shot_type = nil
+testing_distance  = nil
+testing_angle     = nil
+testing_assisted  = nil
+testing_location  = nil
+testing_all_shots = nil
+testing_type_and_distance = nil
+testing_shooter_and_type  = nil
+
 brier = {
     :shot_type => 0,
     :location  => 0,
     :distance  => 0,
     :angle     => 0,
     :assisted  => 0,
-    :all_shots => 0
+    :all_shots => 0,
+    :type_and_distance => 0,
+    :shooter_and_type  => 0
 }
 
 CSV.open("shot_results.csv","wb") do |csv|
-    csv << ['games', 'shot_type', 'distance', 'angle', 'assisted', 'all_shots']
+    csv << ['games', 'shot_type', 'distance', 'location', 'angle', 'assisted', 'all_shots', 'type_and_distance', 'shooter_and_type']
 end
 
 files.each do |file|
@@ -81,11 +102,19 @@ files.each do |file|
 
                 all_shots[:attempts] += 1
                 all_shots[:made] += 1 unless row['result'] == 'missed'
+
+                type_and_distance[type][length][:attempts] += 1
+                type_and_distance[type][length][:made] += 1 unless row['result'] == 'missed'
+
+                shooter = row['player']
+                shooter_and_type[shooter][type][:attempts] += 1
+                shooter_and_type[shooter][type][:made] += 1 unless row['result'] == 'missed'
             end
         end
     end
     game_counter += 1
-    if game_counter.modulo(100) == 0  && !testing
+    if game_counter.modulo(50) == 0  && !testing
+        puts game_counter
         testing           = true
         test_game_counter = 0
         shot_counter      = 0
@@ -95,12 +124,16 @@ files.each do |file|
         testing_assisted  = assisted.clone
         testing_location  = location.clone
         testing_all_shots = all_shots.clone
+        testing_type_and_distance = type_and_distance.clone
+        testing_shooter_and_type  = shooter_and_type.clone
         brier[:shot_type] = 0
         brier[:distance]  = 0
         brier[:angle]     = 0
         brier[:assisted]  = 0
         brier[:location]  = 0
         brier[:all_shots] = 0
+        brier[:type_and_distance] = 0
+        brier[:shooter_and_type]  = 0
     elsif testing
         CSV.foreach(file, headers: true) do |row|
             if row['etype'] == 'shot'
@@ -121,20 +154,30 @@ files.each do |file|
 
                 all_shot_percentage = percentage(testing_all_shots)
 
+                type_and_distance_percentage = percentage(testing_type_and_distance[type][length])
+
+                shooter = row['player']
+                shooter_and_type_percentage = percentage(testing_shooter_and_type[shooter][type])
+
                 brier[:shot_type] += (shot_type_percent   - result)**2
                 brier[:distance]  += (distance_percent    - result)**2
                 brier[:angle]     += (shot_angle_percent  - result)**2
                 brier[:assisted]  += (assisted_percent    - result)**2
                 brier[:location]  += (location_percent    - result)**2
                 brier[:all_shots] += (all_shot_percentage - result)**2
+                brier[:type_and_distance] += (type_and_distance_percentage - result)**2
+                brier[:shooter_and_type] += (shooter_and_type_percentage - result)**2
                 shot_counter += 1
             end
         end
         test_game_counter += 1
-        if test_game_counter == TEST_GAMES
+        if test_game_counter == 10
             CSV.open("shot_results.csv","a+") do |csv|
+                puts "added #{game_counter-test_game_counter} to CSV"
                 csv << [game_counter-test_game_counter, brier[:shot_type]/shot_counter, brier[:distance]/shot_counter,
-                        brier[:angle]/shot_counter, brier[:assisted]/shot_counter, brier[:all_shots]/shot_counter]
+                        brier[:location]/shot_counter, brier[:angle]/shot_counter,
+                        brier[:assisted]/shot_counter, brier[:all_shots]/shot_counter,
+                        brier[:type_and_distance]/shot_counter, brier[:shooter_and_type]/shot_counter]
             end
             testing = false
         end
